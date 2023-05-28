2d21b3b371efd608574ee7bd5bedca55886fdba1
elahemosavi1306 <134330550+elahemosavi1306@users.noreply.github.com>
2023-05-28 11:34:35 +0330

    Add files via upload

---
+++ film.py
@@ -0,0 +1,11 @@
+def film_sinamaee(num1):
+    print(f'the name of the movie is:{num1}')
+    return
+
+def lst(list1):
+        s=sum(list1)
+    print(f'the total emount of selling is:>>{s}')
+    return
+
+def maximom(list1):
+    print(f"the highest amount of selling is:>>{max(list1)}")
